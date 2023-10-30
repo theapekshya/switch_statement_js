7d9d7d134bbb7b59635296f430ca4619a55393c6
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-10-30 22:13:18 +0545

    switch_statement
    Js

---
+++ third.js
@@ -0,0 +1,28 @@
+// switch statement in js 
+
+let input;
+input = 1;
+switch(input)
+{
+    case 1://IF(input===1)
+       document.write("continue");
+       break;
+
+case "y"://if(input==="y")
+      document.write("continue");
+
+case "yes":
+    document.write("continue");
+    break;
+
+case 0:
+    document.write("end");
+    break;
+
+case "n":
+    document.write("end");
+    break;
+
+default:
+    document.write("wrong input");
+}
